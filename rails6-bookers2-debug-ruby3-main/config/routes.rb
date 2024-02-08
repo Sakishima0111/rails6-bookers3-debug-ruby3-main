549befe7d191020c761a66e06c268748dfc2fe07
EC2 Default User <saki120428@gmail.com>
2024-02-08 07:49:52 +0000

    group-2

--- rails6-bookers2-debug-ruby3-main/config/routes.rb
+++ rails6-bookers2-debug-ruby3-main/config/routes.rb
@@ -21,7 +21,9 @@ Rails.application.routes.draw do
   end
 
   resources :notifications
-  resources :groups, only: [:new, :index, :show, :create, :edit, :update]
+  resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
+    resource :group_users, only: [:create, :destroy]
+  end
   resources :chats, only: [:show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
