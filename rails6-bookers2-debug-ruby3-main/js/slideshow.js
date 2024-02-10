4138a9ac7bf3197f5321ff5fc1767a6fb01230ea
EC2 Default User <saki120428@gmail.com>
2024-02-10 14:53:31 +0000

    slideshow1

---
+++ rails6-bookers2-debug-ruby3-main/js/slideshow.js
@@ -0,0 +1,14 @@
+$(function(){
+  $('.slideshow').each(function(){
+    var $slides =$(this).find('img'),
+        slideCount =$slides.length,
+        currentIndex= 0;
+    $slides.eq(showNextSlide, 7500);
+    function showNextSlide(){
+      var nextIndex=(currentIndex +1)%slideCount;
+      $slides.eq(currentIndex).fadeOut();
+      $slides.eq(nextIndex).fadeIn();
+      currentIndex=nextIndex;
+    }
+  });
+});
